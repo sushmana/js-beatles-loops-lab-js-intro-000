22af9b89b462bf0ad58cc5844f53b515e251724f
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-14 17:17:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+var musiciansArray=new array();
+var instrumentsArray=new array();
+
+
+function theBeatlesPlay(musiciansArray,instrumentsArray){
+  var arr=new array();
+  for(let i=0;i<=musicianArray.length;i++){
+    for(let j=0;j<=instrumentsArray.length;j++){
+      console.log(`${i} plays ${j}`);
+    }
+  }
+}
+
+function johnLennonFacts(){
+  
+}
