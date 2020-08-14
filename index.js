fd75fc10d3557532a5b6b7f123b35a112b31ef84
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-14 17:25:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var musiciansArray=new array();
 var instrumentsArray=new array();
-
+var number;
+const facts= [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+const newFacts=new array();
+const emptyArray=new array();
 
 function theBeatlesPlay(musiciansArray,instrumentsArray){
   var arr=new array();
@@ -11,6 +19,18 @@ function theBeatlesPlay(musiciansArray,instrumentsArray){
   }
 }
 
-function johnLennonFacts(){
+
+function johnLennonFacts(facts){
+var  a=!!!;
+int i=0;
+ while(i<=facts.length){
+   newFacts=console.log(`${facts[i]} ${a}`);
+   ++i;
+ }
+ return newFacts;
+}
+
+
+function iLoveTheBeatles(number){
   
 }
